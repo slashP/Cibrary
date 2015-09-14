7cfb718a2f5a7b8090218a747597575b860074e5
Ragnhild Kosmo Holm <ragnhild.kosmo.holm@Ciber.com>
2015-09-14 15:50:37 +0200

    Added book controller and view

---
+++ Cibrary/src/Cibrary/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Cibrary.Controllers
+{
+    public class BookController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
